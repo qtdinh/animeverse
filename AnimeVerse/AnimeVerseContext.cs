dc40f42f2386403b9f90b6302c8bfba145895bd5
Thomas Dinh <thomas98dinh@gmail.com>
2023-11-30 20:37:59 -0800

    Fix connection string error

--- AnimeVerse/AnimeVerseContext.cs
+++ AnimeVerse/AnimeVerseContext.cs
@@ -23,8 +23,15 @@ public partial class AnimeVerseContext : IdentityDbContext<AnimeVerseUser>
     public virtual DbSet<Series> Series { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("DefaultConnection");
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        IConfiguration configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString(name: "AnimeVerseContext"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
