a82a499217fcf4dc5ea0314ad18e12bc375bac92
Thomas Dinh <thomas98dinh@gmail.com>
2023-12-14 21:00:43 -0800

    New migrations

---
+++ AnimeVerse/Migrations/20231215044107_NewChanges.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AnimeVerse.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "genre",
+                table: "Series");
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "Character");
+            migrationBuilder.AddColumn<string>(
+                name: "Demographic",
+                table: "Character",
+                type: "varchar(50)",
+                nullable: false,
+                defaultValue: "");
+            migrationBuilder.RenameTable(
+               name: "Character",          // Old table name
+               newName: "Characters"       // New table name
+           );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+               name: "Demographic",
+               table: "Character");
+
+            migrationBuilder.RenameTable(
+                name: "Characters",       // New table name
+                newName: "Character"      // Old table name
+            );
+
+            migrationBuilder.AddColumn<string>(
+                name: "genre",
+                table: "Series",
+                type: "string",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "Character",
+                type: "string",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
